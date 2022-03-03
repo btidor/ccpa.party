2f54ab6669fc918fae75155a433f1a8b1fdbee59
Benjamin Tidor <benjamin@tidor.net>
2022-03-03 15:46:10 -0800

    Start simple homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const SUPPORTED_PROVIDERS = [
+  'Slack', 'Google', 'Facebook', 'Amazon',
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">
+        <a href="#">🎉 ccpa.party</a>
+      </h1>
+      <section>
+        Upload data from...
+        <ul>
+          {SUPPORTED_PROVIDERS.map(provider =>
+            <li><a href="#">{provider}</a></li>
+          )}
+        </ul>
+      </section>
     </div>
   );
 }
