4da3ef5b6a81efb58f6774e284db38af9b8e0ce2
Benjamin Tidor <benjamin@tidor.net>
2022-04-08 17:15:20 -0700

    Add Cross-Origin headers for extra process isolation

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,8 @@ module.exports = {
     headers: {
       "Content-Security-Policy":
         "default-src 'self' 'unsafe-inline' blob:; form-action 'none'; sandbox allow-scripts allow-same-origin allow-popups; style-src 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
 };
