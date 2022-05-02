6c2389375d1b88a6a911d764c962d5c2198acc5b
Benjamin Tidor <benjamin@tidor.net>
2022-05-01 18:46:02 -0700

    wrap up copy on new request mockup

--- src/Request.js
+++ src/Request.js
@@ -19,28 +19,38 @@ function Request(props: Props): React.Node {
       style={{ "--primary": darkColor(provider) }}
     >
       <section>
-        <span className={styles.logo}>
+        <div className={styles.logo}>
           <Logo />
-        </span>
-        <span className={styles.provider}>{provider.displayName} ⏎</span>
-        <span className={styles.item}>
-          <span className={styles.emoji}>👉</span>
-          <pre>
-            {`Google Takeout →
+        </div>
+        <div className={styles.provider}>{provider.displayName} ◆</div>
 
- under "My Activity"
-  click second pill
-   select JSON, not HTML`}
-          </pre>
-        </span>
-        <span className={styles.item}>
+        <div className={styles.instruction}>
+          <span className={styles.emoji}>👉</span>
+          <div className={styles.pointer}>
+            <a href="https://example.org/" target="_blank" rel="noreferrer">
+              Google Takeout →
+            </a>
+            <pre>
+              {`under "My Activity"
+click second pill
+& select JSON`}
+            </pre>
+          </div>
+        </div>
+
+        <div className={styles.instruction}>
           <span className={styles.emoji}>⏳</span>
-          <pre>up to a few days</pre>
-        </span>
-        <span className={styles.item}>
+          <code>results in up to a few days</code>
+        </div>
+
+        <div className={styles.instruction}>
           <span className={styles.emoji}>🧭</span>
-          <pre>[Browse] Select a file...</pre>
-        </span>
+          <div className={styles.import}>
+            <input id="import" type="file" multiple accept=".zip,.tar.gz" />
+            <label for="import">Import Archive ↑</label>
+            <code></code>
+          </div>
+        </div>
       </section>
     </main>
   );
