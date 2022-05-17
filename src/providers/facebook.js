a7b98938c58558dffae07b4af5b45caf9011c703
Benjamin Tidor <benjamin@tidor.net>
2022-05-16 18:50:29 -0700

    polish facebook provider

--- src/providers/facebook.js
+++ src/providers/facebook.js
@@ -1,142 +1,100 @@
 // @flow
+import { DateTime } from "luxon";
 import * as React from "react";
 
 import { getSlugAndDayTime, parseJSON } from "common/parse";
+import { darkColor } from "common/provider";
+import SimpleRecord from "components/SimpleRecord";
 
 import type { DataFile, Entry, TimelineEntry } from "common/database";
 import type { Provider, TimelineCategory } from "common/provider";
 
-const timelineLabels: { [string]: [string, string] } = {
-  "apps_and_websites_off_of_facebook/apps_and_websites.json": [
-    "Added/Removed App",
-    "activity",
-  ],
-  "comments_and_reactions/posts_and_comments.json": ["Comment", "content"],
-  "comments_and_reactions/comments.json": ["Comment", "content"],
-  "events/event_invitations.json": ["Invited to Event", "activity"],
-  "friends_and_followers/friend_requests_received.json": [
-    "Received Friend Request",
-    "activity",
-  ],
-  "friends_and_followers/friends.json": ["Became Friends", "activity"],
-  "friends_and_followers/rejected_friend_requests.json": [
-    "Rejected Friend Request",
-    "activity",
-  ],
-  "friends_and_followers/removed_friends.json": ["Un-Friended", "activity"],
-  "groups/your_comments_in_groups.json": ["Groups", "content"],
-  "groups/your_group_membership_activity.json": ["Groups", "activity"],
-  "groups/your_groups.json": ["Joined Group", "activity"],
-  "groups/your_posts_in_groups.json": ["Groups", "content"],
-  "notifications/notifications.json": ["Notification", "notification"],
-  "pages/pages_you've_unfollowed.json": ["Unfollowed Page", "activity"],
-  "polls/polls_you_voted_on.json": ["Polls", "content"],
-  "search/your_search_history.json": ["Search", "activity"],
-  "security_and_login_information/account_activity.json": [
-    "Security Log",
-    "security",
-  ],
-  "security_and_login_information/email_address_verifications.json": [
-    "Security Log",
-    "security",
-  ],
-  "security_and_login_information/logins_and_logouts.json": [
-    "Security Log",
-    "security",
-  ],
-  "security_and_login_information/ip_address_activity.json": [
-    "Security Log",
-    "security",
-  ],
+const categories = {
+  installed_apps_v2: "activity",
+  comments_v2: "content",
+  reactions_v2: "content",
+  events_invited_v2: "content",
+  received_requests_v2: "content",
+  friends_v2: "content",
+  rejected_requests_v2: "content",
+  deleted_friends_v2: "content",
+  group_comments_v2: "content",
+  groups_joined_v2: "content",
+  groups_admined_v2: "activity",
+  group_posts_v2: "content",
+  notifications_v2: "notification",
+  pages_unfollowed_v2: "activity",
+  poll_votes_v2: "content",
+  account_activity_v2: "security",
+  contact_verifications_v2: "security",
+  used_ip_address_v2: "security",
+  login_protection_data_v2: "security",
+  account_accesses_v2: "security",
+  admin_records_v2: "security",
 };
 
-const settingLabels: { [string]: string } = {
-  "facebook_payments/payment_history.json": "Facebook Payments",
-  "groups/creator_badges.json": "Group Badges",
-  "location/primary_location.json": "Location",
-  "location/timezone.json": "Time Zone",
-  "other_logged_information/friend_peer_group.json": "Friend Peer Group",
-  "profile_information/profile_information.json": "Profile Information",
-  "security_and_login_information/browser_cookies.json": "DATR Cookie",
-  "security_and_login_information/your_facebook_activity_history.json":
-    "Recent Activity",
-  "voting_location_and_reminders/location.json": "Voting Location",
-  "voting_location_and_reminders/voting_reminders.json": "Voting Reminders",
+const mappers = {
+  installed_apps_v2: (item) => [
+    `App ${item.category[0].toUpperCase() + item.category.slice(1)}`,
+    item.name,
+  ],
+  comments_v2: (item) => [
+    "Comment",
+    item.data?.[0]?.comment?.comment || item.title,
+  ],
+  reactions_v2: (item) => ["Reaction", item.title],
+  events_invited_v2: (item) => ["Event Invitation", item.name],
+  received_requests_v2: (item) => ["Friend Request", item.name],
+  friends_v2: (item) => ["Became Friends", item.name],
+  rejected_requests_v2: (item) => ["Rejected Friend Request", item.name],
+  deleted_friends_v2: (item) => ["Unfriended", item.name],
+  group_comments_v2: (item) => [
+    "Comment",
+    item.data?.[0]?.comment?.comment || item.title,
+  ],
+  groups_joined_v2: (item) => [
+    "Joined Group",
+    item.data?.[0]?.name || item.title,
+  ],
+  groups_admined_v2: (item) => ["Became Group Admin", item.name],
+  group_posts_v2: (item) => ["Post", item.data?.[0]?.post || item.title],
+  notifications_v2: (item) => ["Notification", item.text],
+  pages_unfollowed_v2: (item) => [
+    "Un-Followed",
+    item.data?.[0]?.name || item.title,
+  ],
+  poll_votes_v2: (item) => ["Voted on Poll"],
+  account_activity_v2: (item) => [
+    item.action
+      .split(" ")
+      .map((w) => w[0].toUpperCase() + w.slice(1))
+      .join(" "),
+    item.ip_address,
+  ],
+  contact_verifications_v2: (item) => ["Verified Email Address", item.contact],
+  used_ip_address_v2: (item) => [
+    item.action
+      .split(" ")
+      .map((w) => w[0].toUpperCase() + w.slice(1))
+      .join(" "),
+    item.ip,
+  ],
+  login_protection_data_v2: (item) => ["Session", item.name],
+  account_accesses_v2: (item) => [
+    item.action
+      .split(" ")
+      .map((w) => w[0].toUpperCase() + w.slice(1))
+      .join(" "),
+    item.ip_address,
+  ],
+  admin_records_v2: (item) => [
+    item.event
+      .split(" ")
+      .map((w) => w[0].toUpperCase() + w.slice(1))
+      .join(" "),
+    item.session?.ip_address,
+  ],
 };
-
-async function parseFile(
-  file: DataFile
-): Promise<$ReadOnlyArray<TimelineEntry>> {
-  const ext = file.path.slice(-1)[0].split(".").slice(-1)[0];
-  switch (ext) {
-    case "json": {
-      const parsed = parseJSON(file.data);
-
-      const settingLabel = settingLabels[file.path.slice(1).join("/")];
-      if (settingLabel) {
-        return [];
-      }
-
-      const pair = timelineLabels[file.path.slice(1).join("/")];
-      let timelineLabel, category;
-      if (pair) [timelineLabel, category] = pair;
-
-      if (Array.isArray(parsed)) {
-        return parsed
-          .map((entry) => discoverEntry(file, entry, timelineLabel, category))
-          .filter((x) => x);
-      }
-
-      const keys = Object.keys(parsed);
-      if (keys.length === 1 && Array.isArray(parsed[keys[0]])) {
-        return parsed[keys[0]]
-          .map((entry) => discoverEntry(file, entry, timelineLabel, category))
-          .filter((x) => x);
-      }
-      return [];
-    }
-    default: {
-      return [];
-    }
-  }
-}
-
-function discoverEntry(
-  file: DataFile,
-  obj: any,
-  timelineLabel: ?string,
-  category: string
-): ?TimelineEntry {
-  const label =
-    obj.name ||
-    obj.title ||
-    obj.text ||
-    obj.action ||
-    obj.event ||
-    obj.advertiser_name ||
-    JSON.stringify(obj);
-
-  let timestamp =
-    obj.timestamp ||
-    obj.timestamp_ms ||
-    obj.verification_time ||
-    obj.start_timestamp ||
-    obj.added_timestamp ||
-    obj.removed_timestamp;
-  if (timestamp > 9999999999) timestamp /= 1000;
-
-  return timestamp
-    ? {
-        type: "timeline",
-        provider: file.provider,
-        file: file.path,
-        category,
-        ...getSlugAndDayTime(timestamp, obj),
-        context: [timelineLabel || "unknown: " + file.path.join("/"), label],
-        value: obj,
-      }
-    : undefined;
-}
 
 class Facebook implements Provider {
   slug: string = "facebook";
@@ -155,24 +113,35 @@ class Facebook implements Provider {
   timelineCategories: $ReadOnlyArray<TimelineCategory> = [
     {
       char: "a",
+      icon: "🖱",
       slug: "activity",
       displayName: "Activity",
       defaultEnabled: true,
     },
     {
       char: "c",
+      icon: "📱",
       slug: "content",
       displayName: "Content",
       defaultEnabled: true,
     },
     {
+      char: "m",
+      icon: "💬",
+      slug: "message",
+      displayName: "Messages",
+      defaultEnabled: true,
+    },
+    {
       char: "n",
+      icon: "🔔",
       slug: "notification",
       displayName: "Notifications",
       defaultEnabled: false,
     },
     {
       char: "s",
+      icon: "🪪",
       slug: "security",
       displayName: "Security Logs",
       defaultEnabled: false,
@@ -180,34 +149,163 @@ class Facebook implements Provider {
   ];
 
   async parse(file: DataFile): Promise<$ReadOnlyArray<Entry>> {
+    const entry = (
+      row: any,
+      category: string,
+      datetime: any,
+      context: any
+    ) => ({
+      type: "timeline",
+      provider: file.provider,
+      file: file.path,
+      category,
+      ...getSlugAndDayTime(datetime.toSeconds(), row),
+      context,
+      value: row,
+    });
+
     if (file.path[1] === "messages") {
-      return []; // TODO: handle messages
+      const filename = file.path.slice(-1)[0];
+      if (filename.startsWith("message_") && filename.endsWith(".json")) {
+        let parsed;
+        try {
+          parsed = parseJSON(file.data);
+        } catch {
+          return [];
+        }
+        return parsed.messages.map((item) =>
+          entry(item, "message", DateTime.fromMillis(item.timestamp_ms), [
+            item.content,
+            parsed.title === item.sender_name ? undefined : parsed.title,
+            item.sender_name,
+          ])
+        );
+      }
     } else if (file.path[1] === "posts") {
-      return []; // TODO: handle posts
-    } else if (!file.path.slice(-1)[0].endsWith(".json")) {
-      return parseFile(file);
-    } else if (
-      file.path.slice(1).join("/") === "events/your_event_responses.json"
-    ) {
+      if (
+        file.path[2].startsWith("your_posts") &&
+        file.path[2].endsWith(".json")
+      ) {
+        const parsed = parseJSON(file.data);
+        return [
+          entry(parsed, "content", DateTime.fromSeconds(parsed.timestamp), [
+            "Post",
+            parsed.data?.[0]?.post,
+          ]),
+        ];
+      }
+    } else if (file.path.slice(-1)[0] === "your_event_responses.json") {
       const parsed = parseJSON(file.data);
       const root = parsed.event_responses_v2;
       return root.events_joined
-        .map((e) => discoverEntry(file, e, "Event [Going]", "activity"))
+        .map((item) =>
+          entry(item, "content", DateTime.fromSeconds(item.start_timestamp), [
+            "Going to Event",
+            item.name,
+          ])
+        )
         .concat(
-          root.events_declined.map((e) =>
-            discoverEntry(file, e, "Event [Declined]", "activity")
+          root.events_declined.map((item) =>
+            entry(item, "content", DateTime.fromSeconds(item.start_timestamp), [
+              "Declined Event",
+              item.name,
+            ])
           )
         );
-    } else {
-      return parseFile(file);
+    } else if (file.path.slice(-1)[0] === "your_off-facebook_activity.json") {
+      return parseJSON(file.data).off_facebook_activity_v2.flatMap((company) =>
+        company.events.map((item) =>
+          entry(item, "activity", DateTime.fromSeconds(item.timestamp), [
+            "Off-Facebook Purchase",
+            company.name,
+          ])
+        )
+      );
+    } else if (file.path.slice(-1)[0] === "feed.json") {
+      return parseJSON(file.data).people_and_friends_v2.flatMap((feed) =>
+        feed.entries.map((item) =>
+          entry(item, "activity", DateTime.fromSeconds(item.timestamp), [
+            feed.name,
+            item.data.name,
+          ])
+        )
+      );
+    } else if (file.path.slice(-1)[0] === "profile_information.json") {
+      const parsed = parseJSON(file.data).profile_v2;
+      return [
+        entry(
+          parsed,
+          "activity",
+          DateTime.fromSeconds(parsed.registration_timestamp),
+          ["Created Profile", parsed.name.full_name]
+        ),
+      ];
+    } else if (file.path.slice(-1)[0] === "recently_viewed.json") {
+      return parseJSON(file.data)
+        .recently_viewed.flatMap((category) =>
+          category.entries?.map(
+            (item) =>
+              item.timestamp &&
+              entry(item, "activity", DateTime.fromSeconds(item.timestamp), [
+                `Viewed ${category.name}`,
+                item.data.name,
+              ])
+          )
+        )
+        .filter((x) => x);
+    } else if (file.path.slice(-1)[0] === "recently_visited.json") {
+      return parseJSON(file.data)
+        .visited_things_v2.flatMap(
+          (category) =>
+            category.name === "Profile visits" &&
+            category.entries.map(
+              (item) =>
+                item.timestamp &&
+                entry(item, "activity", DateTime.fromSeconds(item.timestamp), [
+                  "Viewed Profile",
+                  item.data.name,
+                ])
+            )
+        )
+        .filter((x) => x);
+    } else if (file.path.slice(-1)[0].endsWith(".json")) {
+      return Object.entries(parseJSON(file.data)).flatMap(([key, value]) => {
+        if (Array.isArray(value) && categories[key] && mappers[key]) {
+          return value.map((item: any) =>
+            entry(
+              item,
+              categories[key],
+              DateTime.fromSeconds(
+                item.timestamp ||
+                  item.start_timestamp ||
+                  item.removed_timestamp ||
+                  item.removed_timestamp ||
+                  item.verification_time ||
+                  item.session?.created_timestamp
+              ),
+              mappers[key](item)
+            )
+          );
+        } else {
+          return [];
+        }
+      });
     }
+    return [];
   }
 
-  render(entry: TimelineEntry): React.Node {
+  render(entry: TimelineEntry, time: ?string): React.Node {
+    const [body, trailer, username] = entry.context;
     return (
-      <React.Fragment>
-        ({entry.context[0]}) {entry.context[1]}
-      </React.Fragment>
+      <SimpleRecord
+        time={time}
+        username={username && { display: username, color: darkColor(this) }}
+        icon={
+          this.timelineCategories.find((c) => c.slug === entry.category)?.icon
+        }
+        body={body}
+        trailer={trailer}
+      />
     );
   }
 }
