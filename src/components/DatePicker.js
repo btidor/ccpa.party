36b25ab7a54c31e214a80200ff58143a9655d6bc
Benjamin Tidor <benjamin@tidor.net>
2022-05-19 22:12:08 -0700

    make date picker more responsive

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -21,18 +21,24 @@ const VerboseDateFormat = new Intl.DateTimeFormat("en-US", {
 function DatePicker(props: Props): React.Node {
   const { index, rows, scrollToIndex } = props;
 
-  let node;
+  const ref = React.useRef<?HTMLInputElement>();
+
+  let inner;
   const row = rows?.[index];
   if (row && rows) {
     const parts = row.day.split("-").map((x) => parseInt(x));
     const date = new Date(parts[0], parts[1] - 1, parts[2]);
-    node = (
+    inner = (
       <React.Fragment>
-        <label>
+        <label
+          // $FlowFixMe[prop-missing]
+          onClick={() => ref.current?.showPicker?.()}
+        >
           {VerboseDateFormat.format(date)}
           <input
             type="date"
-            value={row.day}
+            ref={ref}
+            defaultValue={row.day}
             onChange={(e) => {
               let target = 0;
               for (const [index, item] of rows.entries()) {
@@ -48,7 +54,7 @@ function DatePicker(props: Props): React.Node {
     );
   }
 
-  return <div className={styles.picker}>{node}</div>;
+  return <div className={styles.picker}>{inner}</div>;
 }
 
 export default DatePicker;
